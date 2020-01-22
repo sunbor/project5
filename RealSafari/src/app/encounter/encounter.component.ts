beb5e29e373758dce9179c603fe79e52fca5e7d3
Wyatt LaVoie <wyatt.lavoie@revature.net>
2020-01-22 11:04:56 -0500

    refactor encounter component and service

--- RealSafari/src/app/encounter/encounter.component.ts
+++ RealSafari/src/app/encounter/encounter.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { EncounterService } from '../services/encounter.service';
+import { EncounterService, Digimon } from '../services/encounter.service';
 
 const _DIGIDEX_SIZE_: number = 100; // how many different Digimon the API has, hard coded for now
 
@@ -9,9 +9,14 @@ const _DIGIDEX_SIZE_: number = 100; // how many different Digimon the API has, h
   styleUrls: ['./encounter.component.css']
 })
 export class EncounterComponent implements OnInit {
+  private isInEncounter: boolean;
   private catchRate: number; // base rate determined by digimon evolution
   private escapeRate: number; // base rate determined by digimon evolution
-  // private digimon: Digimon; // Will need to feed a digimon into this
+  private digimon: Digimon; // Will need to feed a digimon into this
+  private currentUser$ = { // This is temporary!
+    id: 1,
+    username: "admin",
+  };
 
   constructor(private encounterService: EncounterService) { }
 
@@ -20,41 +25,50 @@ export class EncounterComponent implements OnInit {
 
 
   beginEncounter() {
-    let encounterNumber: number = Math.floor(Math.random()*_DIGIDEX_SIZE_ + 1); // Random generated the Digimon id from 1 to 100.
-    this.encounterService.getEncounter(encounterNumber);
+    let encounterValue: number = Math.floor(Math.random() * _DIGIDEX_SIZE_ + 1); // Random generated the Digimon id from 1 to 100.
+    this.encounterService.getEncounter(encounterValue).subscribe(
+      data => {
+        this.digimon = data;
+        this.isInEncounter = true;
+      },
+      err => {
+        console.log(`An error occured when getting Digimon with id: ${encounterValue}.`)
+        console.log(err.error);
+      }
+    );
   };
 
   throwBall(): void {
-    let catchRoll: number = Math.floor(Math.random()*101); // Random integer between 0 and 100
-    if(catchRoll + (this.catchRate * 5) >= 100){ // 100% catch rate
+    let catchRoll: number = Math.floor(Math.random() * 101); // Random integer between 0 and 100
+    if (catchRoll + (this.catchRate * 5) >= 100) { // 100% catch rate
       // Call successful catch method
     }
   }
 
-  throwBait(): void{
-    this.catchRate = this.catchRate - Math.floor(Math.random()*5 + 1); // Reduce catch chance by anywhere from 5% to 25%
-    this.escapeRate = this.escapeRate - Math.floor(Math.random()*5 + 1); // Reduce escape chance by anywhere from 5% to 25%
+  throwBait(): void {
+    this.catchRate = this.catchRate - Math.floor(Math.random() * 5 + 1); // Reduce catch chance by anywhere from 5% to 25%
+    this.escapeRate = this.escapeRate - Math.floor(Math.random() * 5 + 1); // Reduce escape chance by anywhere from 5% to 25%
 
-    if(this.catchRate < 1) {
+    if (this.catchRate < 1) {
       this.catchRate = 1;
     }
 
-    if(this.escapeRate < 0){
+    if (this.escapeRate < 0) {
       this.escapeRate = 1;
     }
 
     this.rollForEscape();
   }
 
-  throwRock(): void{
+  throwRock(): void {
     this.catchRate = this.catchRate + 3; // Increase catch chance by 15%
-    this.escapeRate = this.escapeRate + Math.floor(Math.random()*4 + 1); // Increase escape chance by anywhere from 5% to 20%
+    this.escapeRate = this.escapeRate + Math.floor(Math.random() * 4 + 1); // Increase escape chance by anywhere from 5% to 20%
 
-    if(this.catchRate > 20){
+    if (this.catchRate > 20) {
       this.catchRate = 20;
     }
 
-    if(this.escapeRate > 20){
+    if (this.escapeRate > 20) {
       this.escapeRate = 20;
     }
 
@@ -62,10 +76,25 @@ export class EncounterComponent implements OnInit {
   }
 
   rollForEscape(): void {
-    let escapeRoll: number = Math.floor(Math.random()*101); // Random integer between 0 and 100
-    if((escapeRoll - 100 + (this.escapeRate * 5)) > 0) {
+    let escapeRoll: number = Math.floor(Math.random() * 101); // Random integer between 0 and 100
+    if ((escapeRoll - 100 + (this.escapeRate * 5)) > 0) {
       // Digimon will flee
-    } 
+    }
     // Add some logic to give a hint to the user if the Digimon is close to fleeing and/or has a high catch rate
   }
+
+  saveDigimon() {
+    let saveInfo = {
+      "user": `${this.currentUser$}`, // will send current user's info
+      "digimon": `${this.digimon}` // will send the Digimon's data
+    }
+    this.encounterService.saveCatch(saveInfo).subscribe(
+      data => {
+        console.log("Server responded with success code", data); // This might not return a success code. Will need to be tested
+      },
+      err => {
+        console.log(err.error);
+      });
+  }
+
 }
--- RealSafari/src/app/services/encounter.service.ts
+++ RealSafari/src/app/services/encounter.service.ts
@@ -1,43 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from '../model/user.model';
 
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class EncounterService {
-  private digimon: Digimon;
-  private currentUser$: User;
+export class EncounterService {  
 
   constructor(private http: HttpClient) { }
 
-  getEncounter(encounterNumber: number) {
-    return this.http.get<Digimon>(`https://digimon-api.herokuapp.com/api/digimon/${encounterNumber}`).subscribe(
-      data =>{
-        this.digimon = data;
-      },
-      err => {
-        console.log(`An error occured when getting Digimon with id: ${encounterNumber}.`)
-        console.log(err.error);
-      }
-    );
+  getEncounter(encounterValue: number) {
+  return this.http.get<Digimon>(`https://digimon-api.herokuapp.com/api/digimon/id/${encounterValue}`);
   }
 
-  saveCatch(digimon: Digimon) { // Followed an April 2019 Angular example on this one. 
-    this.http.post("http://localhost:8080/project5/users/digimon", 
-    {
-      "user":`${this.currentUser$}`,
-      "digimon": `${digimon}`
-    }).subscribe(
-      data => {
-        console.log("Server responded with success code", data);
-      },
-      err => {
-        console.log(`Error saving Digimon to ${this.currentUser$.username}: ${digimon}`);
-        console.log(err.error);
-      });
+  endEncounter() {
+  }
+
+  saveCatch(credentials) { // Followed an April 2019 Angular example on this one
+    return this.http.post("http://localhost:8080/project5/users/digimon", credentials, { withCredentials: true });
   }
 
 }
