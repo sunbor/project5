cdf257ebff79703b59ff9184d5a0a3672019b47a
helppaw <53380494+helppaw@users.noreply.github.com>
2020-01-22 10:03:12 -0500

    time to piece it all together

--- project5/src/main/java/com/revature/services/UserService.java
+++ project5/src/main/java/com/revature/services/UserService.java
@@ -1,10 +1,34 @@
 package com.revature.services;
 
+import java.util.List;
+
 import com.revature.models.User;
 
 public class UserService {
+	
+	// change this as you need to
 
 	public static User findById(int id) {
 		return null; //connect to Database here
+		
+	}
+
+	public boolean ifUserExist(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void save(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(User currentUser) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public List<User> findAll() {
+		return null;
 	}
 }
