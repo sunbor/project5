6f0a602ed345b9e64237138003049593c0833143
Wyatt LaVoie <wyatt.lavoie@revature.net>
2020-01-22 14:52:22 -0500

    refactor URIs, catch and escape rate values

--- RealSafari/src/app/encounter/encounter.component.ts
+++ RealSafari/src/app/encounter/encounter.component.ts
@@ -2,18 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { EncounterService, Digimon } from '../services/encounter.service';
 
 const _DIGIDEX_SIZE_: number = 100; // how many different Digimon the API has, hard coded for now
-const _FRESH_CATCH_RATE_: number = 10;
-const _FRESH_ESCAPE_RATE_: number = 1;
-const _IN_TRAINING_CATCH_RATE_: number = 9; // base catch rate of Digimon with level "In Training"
-const _IN_TRAINING_ESCAPE_RATE_: number = 2; // base escape rate of Digimon with level "In Training"
-const _ROOKIE_CATCH_RATE_: number = 7;
-const _ROOKIE_ESCAPE_RATE_: number = 3;
-const _CHAMPION_CATCH_RATE_: number = 5;
-const _CHAMPION_ESCAPE_RATE_: number = 5;
-const _ULTIMATE_CATCH_RATE_: number = 3;
-const _ULTIMATE_ESCAPE_RATE_: number = 7;
-const _MEGA_CATCH_RATE_: number = 2;
-const _MEGA_ESCAPE_RATE_: number = 9;
+const _FRESH_CATCH_RATE_: number = 50;
+const _FRESH_ESCAPE_RATE_: number = 5;
+const _IN_TRAINING_CATCH_RATE_: number = 45; // base catch rate of Digimon with level "In Training"
+const _IN_TRAINING_ESCAPE_RATE_: number = 10; // base escape rate of Digimon with level "In Training"
+const _ROOKIE_CATCH_RATE_: number = 35;
+const _ROOKIE_ESCAPE_RATE_: number = 15;
+const _CHAMPION_CATCH_RATE_: number = 25;
+const _CHAMPION_ESCAPE_RATE_: number = 25;
+const _ULTIMATE_CATCH_RATE_: number = 15;
+const _ULTIMATE_ESCAPE_RATE_: number = 35;
+const _MEGA_CATCH_RATE_: number = 10;
+const _MEGA_ESCAPE_RATE_: number = 45;
 
 @Component({
   selector: 'app-encounter',
@@ -76,30 +76,30 @@ export class EncounterComponent implements OnInit {
 
   throwBall(): void {
     let catchRoll: number = Math.floor(Math.random() * 101); // Random integer between 0 and 100
-    if (catchRoll >= 100 - (this.catchRate * 5)) { // successfully catches a pokemon if the roll is greater than the catch rate threshold
+    if (catchRoll >= 100 - this.catchRate) { // successfully catches a pokemon if the roll is greater than the catch rate threshold
                                                   // if catchRate = 10, then catchRoll must be at least 50 to catch the Digimon
       console.log(`Caught the Digimon ${this.digimon[0].name}!`);
       this.saveDigimon();
     } else {
       console.log(`Failed to catch ${this.digimon[0].name}!`)
       this.escapeRate = this.escapeRate + 2; // Increases the escape chance by 10% if capture fails
-      if (this.escapeRate > 20) {
-        this.escapeRate = 20;
+      if (this.escapeRate > 100) {
+        this.escapeRate = 100;
       }
       this.rollForEscape();
     }
   }
 
   throwBait(): void {
-    this.catchRate = this.catchRate - Math.floor(Math.random() * 5 + 1); // Reduce catch chance by anywhere from 5% to 25%
-    this.escapeRate = this.escapeRate - Math.floor(Math.random() * 5 + 1); // Reduce escape chance by anywhere from 5% to 25%
+    this.catchRate = this.catchRate - (Math.floor(Math.random() * 5 + 1) * 5); // Reduce catch chance by anywhere from 5% to 25%
+    this.escapeRate = this.escapeRate - (Math.floor(Math.random() * 5 + 1) * 5); // Reduce escape chance by anywhere from 5% to 25%
 
-    if (this.catchRate < 1) {
-      this.catchRate = 1;
+    if (this.catchRate < 5) {
+      this.catchRate = 5;
     }
 
     if (this.escapeRate < 0) {
-      this.escapeRate = 1;
+      this.escapeRate = 5;
     }
 
     console.log(this.catchRate);
@@ -108,15 +108,15 @@ export class EncounterComponent implements OnInit {
   }
 
   throwRock(): void {
-    this.catchRate = this.catchRate + 3; // Increase catch chance by 15%
-    this.escapeRate = this.escapeRate + Math.floor(Math.random() * 4 + 1); // Increase escape chance by anywhere from 5% to 20%
+    this.catchRate = this.catchRate + 15; // Increase catch chance by 15%
+    this.escapeRate = this.escapeRate + (Math.floor(Math.random() * 4 + 1) * 5); // Increase escape chance by anywhere from 5% to 20%
 
-    if (this.catchRate > 20) {
-      this.catchRate = 20;
+    if (this.catchRate > 100) {
+      this.catchRate = 100;
     }
 
-    if (this.escapeRate > 20) {
-      this.escapeRate = 20;
+    if (this.escapeRate > 100) {
+      this.escapeRate = 100;
     }
     console.log(this.catchRate);
     console.log(this.escapeRate);
@@ -125,7 +125,7 @@ export class EncounterComponent implements OnInit {
 
   rollForEscape(): void {
     let escapeRoll: number = Math.floor(Math.random() * 101); // Random integer between 0 and 100
-    if ((escapeRoll > 100 - (this.escapeRate * 5))) {
+    if (escapeRoll > 100 - this.escapeRate) {
       console.log("Digimon escaped!")
       this.isInEncounter = false;
     }
