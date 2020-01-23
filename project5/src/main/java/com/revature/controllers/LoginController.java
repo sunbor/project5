83a2fd7b5bb1460f76acfc809a392fc8d1371218
curecoma <ctc.cheatz@gmail.com>
2020-01-22 20:25:13 -0500

    I tried

--- project5/src/main/java/com/revature/controllers/LoginController.java
+++ project5/src/main/java/com/revature/controllers/LoginController.java
@@ -1,5 +1,7 @@
 package com.revature.controllers;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.revature.models.LoginInfo;
 import com.revature.models.User;
+import com.revature.repositories.IUserDao;
 
 @Controller
 @CrossOrigin
 public class LoginController {
+
+	ApplicationContext ac = new ClassPathXmlApplicationContext("applicationcontext.xml");
+	IUserDao dao = (IUserDao) ac.getBean("userDao");
 
 	// Log in, send back user data, and set session
 	// this looks good, explore web.xml to avoid CORS errors
@@ -22,7 +28,7 @@ public class LoginController {
 		System.out.println("we're here");
 		String username = u.getUsername();
 		String password = u.getPassword();
-		User loggedIn = UserDao.getByUsernameAndPassword(username, password);
+		User loggedIn = dao.getByUsernameAndPassword(username, password);
 		if (loggedIn == null) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		} else {
